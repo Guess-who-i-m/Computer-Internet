ba78901e464ffb4fddd9511919dd6882a7d0cde9
Guess-who-i-m <1642166930xkb@gmail.com>
2024-10-17 10:35:16 +0800

    上传完成了使用序列号编码确认的sr协议

--- lab3/run_sr.py
+++ lab3/run_sr.py
@@ -8,7 +8,7 @@ CLIENT_IP = '127.0.0.1'
 CLIENT_PORT = 12346
 
 # 模拟要传输的数据
-data_list = [f"Data{i}" for i in range(16)]
+data_list = [f"Data{i}" for i in range(50)]
 
 Thread_Server = threading.Thread(target=sr.server_program, args=(SERVER_IP, SERVER_PORT, CLIENT_IP, CLIENT_PORT, data_list, ))
 Thread_Client = threading.Thread(target=sr.client_program, args=(CLIENT_IP, CLIENT_PORT, SERVER_IP, SERVER_PORT, ))
